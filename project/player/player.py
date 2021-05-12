ecf610ccdf4210224adfa5c036ffc67c8b4e19e0
felipemeriga <felipe.meriga@gmail.com>
2021-05-12 09:36:49 -0300

    Adding some methods to Player class, for adding the attributes from job and class

--- project/player/player.py
+++ project/player/player.py
@@ -1,5 +1,10 @@
+from project.player.job import Job, Knight
+
+
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, job, race):
+        self.job = job
+        self.race = race
         self.name = name
         self.health_points = 10
         self.magic_points = 10
@@ -9,14 +14,28 @@ class Player:
         self.accuracy = 4
         self.armour = 2
         self.magic_resist = 2
-        self.level = 1
         self.will = 2
+        self.level = 1
+        self.add_attributes(self.job)
+        self.add_attributes(self.race)
+
+    def add_attributes(self, attributes=None):
+        self.health_points += attributes.health_points
+        self.magic_points += attributes.magic_points
+        self.move_speed += attributes.move_speed
+        self.strength += attributes.strength
+        self.intelligence += attributes.intelligence
+        self.accuracy += attributes.accuracy
+        self.armour += attributes.armour
+        self.magic_resist += attributes.magic_resist
+        self.will += attributes.will
+
 
 class ControlledPlayer(Player):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, job, race):
+        super().__init__(name, job, race)
 
 
 class BotPlayer(Player):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, job, race):
+        super().__init__(name, job, race)
