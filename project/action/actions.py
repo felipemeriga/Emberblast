ee499f681dd0de42b273024bb2d1fd7b725ddd95
felipemeriga <felipe.meriga@gmail.com>
2021-05-14 21:48:22 -0300

    Adding actions classes for the game

---
+++ project/action/actions.py
@@ -0,0 +1,41 @@
+class Action:
+    def __init__(self):
+        pass
+
+    def act(self):
+        pass
+
+
+class Move(Action):
+    def __init__(self):
+        pass
+
+
+class Defend(Action):
+    def __init__(self):
+        pass
+
+
+class Hide(Action):
+    def __init__(self):
+        pass
+
+
+class Search(Action):
+    def __init__(self):
+        pass
+
+
+class Atack(Action):
+    def __init__(self):
+        pass
+
+
+class Skill(Action):
+    def __init__(self):
+        pass
+
+
+class Item(Action):
+    def __init__(self):
+        pass
