0efb4af48e344e34cca365cf2250d00a2a08ea22
felipemeriga <felipe.meriga@gmail.com>
2021-05-24 09:57:23 -0300

    Adding some descriptions to the actions

--- project/action/actions.py
+++ project/action/actions.py
@@ -1,3 +1,20 @@
+"""
+The plan here would be, as actions controls the game,
+there will be a Singleton class called actions coordinator, that will
+manage all the plays for all the players.
+
+As the actions can be the same for each of the players, they will be considered Singletons
+too.
+
+Another thing to remember when creating the actions, is to use function generators and yield
+for coordinating the move + another action per turn for each player.
+
+Also, before this, we need to think about how it would be the difference between single player and
+multiplayer, considering that it's ideal to use the same game class structure, to avoid copying similar
+functionalities
+"""
+
+
 class SingletonAction(type):
     _instances = {}
 
