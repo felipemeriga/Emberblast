a5448857fe004f03e08f53d1d62ea595ffca816c
felipemeriga <felipe.meriga@gmail.com>
2021-06-03 08:56:55 -0500

    Changing graph/matrix representation to alphabet letters for rows, and numbers for columns

--- project/map/graph.py
+++ project/map/graph.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple
 
-from project.utils import generate_random_adjacent_matrix, generate_visited_default_matrix
+from project.utils import generate_random_adjacent_matrix, generate_visited_default_matrix, convert_number_to_letter
 
 
 class Edge:
@@ -48,9 +48,13 @@ class Graph:
         if row >= self.size or column >= self.size or visited[row][column]:
             return
         visited[row][column] = True
-        vertex_id = "n" + str(row) + str(column)
-
-        vertex = Vertex(vertex_id, matrix[row][column])
+        letter_row = convert_number_to_letter(row)
+        vertex_id = letter_row + str(column)
+        position = {
+            'row': letter_row,
+            'column': column
+        }
+        vertex = Vertex(vertex_id, matrix[row][column], position)
         if vertex.value == 1:
             self._compute_vertical_edges(vertex, row, column, matrix)
             self._compute_horizontal_edges(vertex, row, column, matrix)
@@ -64,44 +68,44 @@ class Graph:
         # upper edge
         if row - 1 >= 0:
             if matrix[row - 1][column] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row - 1) + str(column), 1)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row - 1) + str(column), 1)
                 vertex.add_edge(edge)
         # lower
         if row + 1 < self.size:
             if matrix[row + 1][column] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row + 1) + str(column), 1)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row + 1) + str(column), 1)
                 vertex.add_edge(edge)
 
     def _compute_horizontal_edges(self, vertex: Vertex, row: int, column: int, matrix: List[List[int]]):
         # left edge
         if column - 1 >= 0:
             if matrix[row][column - 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row) + str(column - 1), 1)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row) + str(column - 1), 1)
                 vertex.add_edge(edge)
         # right edge
         if column + 1 < self.size:
             if matrix[row][column + 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row) + str(column + 1), 1)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row) + str(column + 1), 1)
                 vertex.add_edge(edge)
 
     def _compute_diagonal_edges(self, vertex: Vertex, row: int, column: int, matrix: List[List[int]]):
         if column - 1 >= 0 and row - 1 >= 0:
             if matrix[row - 1][column - 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row - 1) + str(column - 1), 1.414)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row - 1) + str(column - 1), 1.414)
                 vertex.add_edge(edge)
         if column + 1 < self.size and row + 1 < self.size:
             if matrix[row + 1][column + 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row + 1) + str(column + 1), 1.414)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row + 1) + str(column + 1), 1.414)
                 vertex.add_edge(edge)
 
         if column - 1 >= 0 and row + 1 < self.size:
             if matrix[row + 1][column - 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row + 1) + str(column - 1), 1.414)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row + 1) + str(column - 1), 1.414)
                 vertex.add_edge(edge)
 
         if row - 1 >= 0 and column + 1 < self.size:
             if matrix[row - 1][column + 1] == 1:
-                edge = Edge(vertex.vertex_id, "n" + str(row - 1) + str(column + 1), 1.414)
+                edge = Edge(vertex.vertex_id, convert_number_to_letter(row - 1) + str(column + 1), 1.414)
                 vertex.add_edge(edge)
 
     def get_edges_as_array_of_tuples(self) -> List[Tuple[int, int]]:
