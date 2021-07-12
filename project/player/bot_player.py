1909cf4676fc480a3b5dcfa757f9b40cb6c852f3
felipemeriga <felipe.meriga@gmail.com>
2021-07-12 10:02:14 +0200

    Adding docstrings to player package

--- project/player/bot_player.py
+++ project/player/bot_player.py
@@ -1,8 +1,8 @@
 from random import randrange
 
 from project.questions import improve_attributes_automatically
-from .race import dynamic_races_classes
-from .job import dynamic_jobs_classes
+from .race import dynamic_races_classes, Race
+from .job import dynamic_jobs_classes, Job
 from .player import Player
 from project.conf import get_configuration
 from project.utils import JOBS_SECTION, RACES_SECTION
@@ -10,16 +10,37 @@ from project.utils.name_generator import generate_name
 
 
 class BotPlayer(Player):
-    def __init__(self, job, race, name=None):
+    def __init__(self, job: Job, race: Race, name: str =None) -> None:
+        """
+        Constructor of bot player.
+
+        :param Job job: The selected job.
+        :param Race race: The selected race.
+        :param str name: Player's name.
+        :rtype: None.
+        """
         super().__init__(name, job, race)
 
-    def _level_up(self):
+    def _level_up(self) -> None:
+        """
+        Implementation this method from Player class, this function will improve the attribute
+        in an automated way, every time a bot player levels up, considering its race and job, to
+        promote the proper combination of attributes.
+
+        :rtype: None.
+        """
         improvements = improve_attributes_automatically(self.job.get_name(), self.race.get_name())
         for key, value in improvements.items():
             self.__setattr__(key, value + self.__getattribute__(value))
 
 
 def bot_factory(number_of_bots):
+    """
+    Function that will generated all the bots, depending of the number that was informed as the argument,
+    each bot race and job, will be picked randomly.
+
+    :rtype: None.
+    """
     bots = []
     jobs = list(get_configuration(JOBS_SECTION).keys())
     races = list(get_configuration(RACES_SECTION).keys())
--- project/player/controlled_player.py
+++ project/player/controlled_player.py
@@ -1,12 +1,28 @@
+from .job import Job
 from .player import Player
 from project.questions import ask_attributes_to_improve
+from .race import Race
 
 
 class ControlledPlayer(Player):
-    def __init__(self, name, job, race):
+    def __init__(self, job: Job, race: Race, name: str = None) -> None:
+        """
+        Constructor of bot player.
+
+        :param Job job: The selected job.
+        :param Race race: The selected race.
+        :param str name: Player's name.
+        :rtype: None.
+        """
         super().__init__(name, job, race)
 
-    def _level_up(self):
+    def _level_up(self) -> None:
+        """
+        Implementation this method from Player class, this function will ask the controlled player
+        which attributes he wants to raise, on level up.
+
+        :rtype: None.
+        """
         improvements = ask_attributes_to_improve()
         for improvement in improvements:
             attribute = improvement.get('attribute', 'health_points')
