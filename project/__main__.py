1b57fb160f7e65a3d297bf5ce6f04f05c7b69a53
felipemeriga <felipe.meriga@gmail.com>
2021-07-24 03:49:07 +0200

    Adding cloudpickle dependency for saving application state, and atexit to main.py to add an exit handler in the application, to save the application

--- project/__main__.py
+++ project/__main__.py
@@ -1,9 +1,14 @@
 import sys
 
 from colorama import Fore
-
+import atexit
 from project.game import GameFactory
 from project.message import print_greetings
+
+
+def exit_handler():
+    print('My application is ending!')
+
 
 def run_project(args):
     game_orchestrator = None
@@ -12,10 +17,12 @@ def run_project(args):
         game_factory = GameFactory()
         game_orchestrator = game_factory.new_game()
         game_orchestrator.execute_game()
+    except KeyboardInterrupt:
+        pass
     except Exception as err:
-        print(Fore.RED + "System shutdown with unexpected error")
-        exit()
+        print(Fore.RED + 'System shutdown with unexpected error')
 
 
 if __name__ == '__main__':
+    atexit.register(exit_handler)
     run_project(sys.argv)
